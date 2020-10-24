c144d23eebe7eae4f7dcac7c64266624cd23128b
banaconda88 <bananaconda88@gmail.com>
2020-10-24 13:52:11 -0400

    Add files via upload

---
+++ unit converter with functions (1).py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+
+def Inches_Centimeters_converter(i):
+    c = i * 2.54
+    print (i, "inches converts to", c, "centimeters.")
+def Kilograms_Pounds_converter(k):
+    p = k * 2.205
+    print (k, "kilograms converts to", p, "pounds.")
+def Fahrenheit_Celcius_converter(f):
+    c = (32 * f - 32) * 5/9
+    print (f, "degrees Fahrenheit converts to", c, "degrees Celcius.")
+
+
+# In[ ]:
+
+
+
+
